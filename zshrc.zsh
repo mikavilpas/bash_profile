3e5f04e71250e7f4f4d362f7d4c3713dd5158685
Mika Vilpas <mika.vilpas@bowsville.fi>
2012-11-26 09:26:15 +0200

    disable git-flow-completion plugin
    badly implemented, don't really even care about it anymore.

--- zshrc.zsh
+++ zshrc.zsh
@@ -30,7 +30,7 @@ ZSH_THEME="sunaku"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git vi-mode lol git-flow)
+plugins=(git vi-mode lol)
 
 source $ZSH/oh-my-zsh.sh
 
