2fb8ec9d4620b302ae29e7569125811b19f0f528
Mika Vilpas <mika.vilpas@bowsville.fi>
2012-12-05 16:25:44 +0200

    enable extended globbing

--- zshrc.zsh
+++ zshrc.zsh
@@ -57,3 +57,7 @@ bindkey '^P' up-line-or-search
 bindkey '^N' down-line-or-search
 bindkey '^R' history-incremental-search-backward
 bindkey '^S' history-incremental-search-forward
+
+# Enable using complicated globbing,
+# such as ^dangerousFile for all but "dangerousFile"
+setopt extendedglob
