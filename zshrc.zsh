cff59a2df0073c51d7bb0e3ca9ba8655bea6eb07
Mika Vilpas <mika.vilpas@gmail.com>
2014-03-18 16:45:55 +0200

    Change zsh theme to blinks

--- zshrc.zsh
+++ zshrc.zsh
@@ -14,7 +14,7 @@ EDITOR="emacsclient --alternate-editor=''"
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="sunaku"
+ZSH_THEME="blinks"
 
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
