c256ae0a9a7412d258ec4dd95139933cb2ccd65f
Mika Vilpas <mika.vilpas@gmail.com>
2014-03-24 10:36:55 +0200

    Whitespace fixes

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,10 +1,8 @@
-{:user {:dependencies
-
-        ;; auto completion
-        ;; https://github.com/alexander-yakushev/compliment
-        [[compliment "0.0.3"]
-
-         ;; namespace management based on current buffer contents
-         ;; https://github.com/technomancy/slamhound
-         [slamhound "1.5.3"]]
+{:user {:dependencies [
+                       ;; auto completion
+                       ;; https://github.com/alexander-yakushev/compliment
+                       [compliment "0.0.3"]
+                       ;; namespace management based on current buffer contents
+                       ;; https://github.com/technomancy/slamhound
+                       [slamhound "1.5.3"]]
         :plugins [[cider/cider-nrepl "0.1.0-SNAPSHOT"]]}}
