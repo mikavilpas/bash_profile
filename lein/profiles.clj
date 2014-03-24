0ef7ef081e53b3207f173035e6151286ce38792c
Mika Vilpas <mika.vilpas@gmail.com>
2014-03-24 08:34:14 +0200

    Add comments and links to dependency repos

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,10 @@
 {:user {:dependencies
+
+        ;; auto completion
+        ;; https://github.com/alexander-yakushev/compliment
         [[compliment "0.0.3"]
+
+         ;; namespace management based on current buffer contents
+         ;; https://github.com/technomancy/slamhound
          [slamhound "RELEASE"]]
         :plugins [[cider/cider-nrepl "0.1.0-SNAPSHOT"]]}}
