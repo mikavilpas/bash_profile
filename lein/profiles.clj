a07123c6f90e283cead912fa70b3486f9b4a30ae
Mika Vilpas <mika.vilpas@gmail.com>
2014-03-27 14:58:42 +0200

    Add vinyasa to lein profile
    I'm not sure what it does yet

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -6,6 +6,23 @@
    [compliment "0.0.3"]
    ;; namespace management based on current buffer contents
    ;; https://github.com/technomancy/slamhound
-   [slamhound "1.5.3"]]
-  :plugins [[cider/cider-nrepl "0.1.0-SNAPSHOT"]]}}
+   [slamhound "1.5.3"]
 
+   [leiningen #=(leiningen.core.main/leiningen-version)]
+
+   ;; This library is required for the way some utilities are
+   ;; installed. Or something.
+   ;; http://z.caudate.me/give-your-clojure-workflow-more-flow/
+   [im.chit/vinyasa "0.1.9"]
+
+   ]
+  :plugins [[cider/cider-nrepl "0.1.0-SNAPSHOT"]]
+  :injections [
+               (require '[vinyasa.inject :as inj])
+               (inj/inject 'clojure.core
+                           '[[vinyasa.inject inject]
+                             [vinyasa.pull pull]
+                             [vinyasa.lein lein]
+                             [vinyasa.reimport reimport]])
+
+               ]}}
