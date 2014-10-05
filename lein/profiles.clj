82875efe7044da2973c7f91a1aa7b5c37e2258e9
Mika Vilpas <mika.vilpas@gmail.com>
2014-10-05 15:02:55 +0300

    Remove compliment
    It doesn't work with clojurescript, and cider's ac does.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,9 +1,6 @@
 {:user
  {:dependencies
   [
-   ;; auto completion
-   ;; https://github.com/alexander-yakushev/compliment
-   [compliment "0.0.3"]
    ;; namespace management based on current buffer contents
    ;; https://github.com/technomancy/slamhound
    [slamhound "1.5.3"]
