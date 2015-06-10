f2599d4025ef8696ad5df64129e497d1bf2c0a33
Mika Vilpas <mika.vilpas@gmail.com>
2015-06-10 21:53:23 +0300

    Add automatic test runners

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user
  {:plugins [[cider/cider-nrepl "0.8.2"]
-            [lein-autoexpect "1.4.0"]]}}
+            [lein-autoexpect "1.4.0"]
+            [quickie "0.3.6"]
+            [com.jakemccrary/lein-test-refresh "0.9.0"]]}}
