0e1e4f4f070f2eaa7b8dba6bbb750df4a74bb804
Mika Vilpas <mika.vilpas@gmail.com>
2015-06-22 21:12:06 +0300

    Fix typo

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,5 +2,5 @@
  {:plugins [[cider/cider-nrepl "0.9.0"]
             [lein-autoexpect "1.4.0"]
             [quickie "0.3.6"]
-            [com.jakemccrary/lein-test-refresh "0.9.0"]
+            [com.jakemccrary/lein-test-refresh "0.9.0"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
