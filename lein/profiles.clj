d27f260ca926e59f1b1d13369ffb592f9e3cf8f1
Mika Vilpas <mika.vilpas@gmail.com>
2015-06-22 21:08:39 +0300

    Fix cider-nrepl not starting in emacs

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.8.2"]
+ {:plugins [[cider/cider-nrepl "0.9.0"]
             [lein-autoexpect "1.4.0"]
             [quickie "0.3.6"]
-            [com.jakemccrary/lein-test-refresh "0.9.0"]]}}
+            [com.jakemccrary/lein-test-refresh "0.9.0"]
+  :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
