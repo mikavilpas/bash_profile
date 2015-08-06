f100a24474dfccf21a78fd1223ed603d8d50bc6f
Mika Vilpas <mika.vilpas@gmail.com>
2015-08-06 15:09:17 +0300

    Upgrade to cider 0.9.1

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.9.0"]
+ {:plugins [[cider/cider-nrepl "0.9.1"]
             [lein-autoexpect "1.4.0"]
             [quickie "0.3.6"]
             [com.jakemccrary/lein-test-refresh "0.9.0"]]
