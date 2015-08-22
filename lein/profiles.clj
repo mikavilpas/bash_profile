60f4a2d41244a73e10fd0c21644128ff84ad6cac
Mika Vilpas <mika.vilpas@gmail.com>
2015-08-22 08:02:58 +0300

    Add refactor-nrepl 1.2.0-SNAPSHOT

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.9.1"]
+ {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
             [lein-autoexpect "1.4.0"]
+            [refactor-nrepl "1.2.0-SNAPSHOT"]
             [quickie "0.3.6"]
             [com.jakemccrary/lein-test-refresh "0.9.0"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
