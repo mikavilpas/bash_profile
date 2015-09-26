5f65dc9136efd03fc1ab4d8a88fa220579ac5e02
Mika Vilpas <mika.vilpas@gmail.com>
2015-09-26 12:59:13 +0300

    Revert "Go back to the stable cider and refactor-nrepl"
    This reverts commit 99f4df4fcd3652805be7ec87b06254c4b103b248.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,7 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.9.1"]
-            [refactor-nrepl "1.1.0"]
-            [cider/cider-nrepl "0.9.1"]
+ {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
             [lein-autoexpect "1.4.0"]
+            [refactor-nrepl "1.2.0-SNAPSHOT"]
             [quickie "0.3.6"]
             [com.jakemccrary/lein-test-refresh "0.9.0"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
+  :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
