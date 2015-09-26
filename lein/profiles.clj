99f4df4fcd3652805be7ec87b06254c4b103b248
Mika Vilpas <mika.vilpas@gmail.com>
2015-09-26 12:44:02 +0300

    Go back to the stable cider and refactor-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,8 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+ {:plugins [[cider/cider-nrepl "0.9.1"]
+            [refactor-nrepl "1.1.0"]
+            [cider/cider-nrepl "0.9.1"]
             [lein-autoexpect "1.4.0"]
-            [refactor-nrepl "1.2.0-SNAPSHOT"]
             [quickie "0.3.6"]
             [com.jakemccrary/lein-test-refresh "0.9.0"]]
-  :dependencies [[org.clojure/tools.nrepl "0.2.7"]]}}
+  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
