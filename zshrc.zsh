736a92add60b3fb97c4291c87209086fd035f521
Mika Vilpas <mika.vilpas@gmail.com>
2016-10-18 09:45:09 +0300

    Add docker plugin

--- zshrc.zsh
+++ zshrc.zsh
@@ -39,7 +39,7 @@ ZSH_THEME="blinks"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git vi-mode lol vagrant)
+plugins=(git vi-mode lol vagrant docker)
 
 source $ZSH/oh-my-zsh.sh
 
