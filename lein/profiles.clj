d1e198d1cd61f6af036433178b23f1d3d5563c1d
Mika Vilpas <mika.vilpas@gmail.com>
2018-01-22 15:51:35 +0200

    Update to cider-nrepl 0.17.0-SNAPSHOT

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user
- {:plugins [[cider/cider-nrepl "0.15.1-SNAPSHOT"]
+ {:plugins [[cider/cider-nrepl "0.17.0-SNAPSHOT"]
             [refactor-nrepl "2.4.0-SNAPSHOT"]]
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
